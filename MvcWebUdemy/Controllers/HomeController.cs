feddeefbd0f69decef4ea4a9676eccd651186026
knolegsb <seon2you@hotmail.com>
2015-12-23 21:19:38 -0800

    start and added model

---
+++ MvcWebUdemy/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MvcWebUdemy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebUdemy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new Person() { Name = "Sean" };
+
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Location = "Los Angeles";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
